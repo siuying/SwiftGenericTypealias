5d244d133ec7cb48f0e2a799f09c890285c9486c
Francis Chong <francis@ignition.hk>
2016-08-03 15:38:11 +0800

    Initial Commit

---
+++ XCode8B4CompilerError/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCode8B4CompilerError
+//
+//  Created by Francis Chong on 8/3/16.
+//  Copyright © 2016 Time Based Technology Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
